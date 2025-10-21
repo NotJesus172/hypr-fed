495400e4be07d304b501725b7ed224ccd25a8cca
NotJesus172 <naftalisilvera@gmail.com>
2025-10-20 21:59:43 -0400

    reordered install process

--- install.sh
+++ install.sh
@@ -10,19 +10,16 @@ sudo echo "fastestmirror=true" >> /etc/dnf/dnf.conf
 sudo dnf -y up
 #   Adding rpmfusion repositories
 sudo dnf -y in https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-#   Install base packages
-sudo dnf -y in nano git curl bash bash-devel wget btop fastfetch firefox kitty dolphin code tmux sddm
+#   Enable COPR Repos
+sudo dnf -y copr enable atim/starship #  Starship
+sudo dnf -y copr enable solopasha/hyprland #  Hyprland
 #   Install NerdFonts
 sudo unzip -n CodeNewRoman.zip -d ~/.local/share/fonts/
 sudo unzip -n ComicShannsMono.zip -d ~/.local/share/fonts/
 sudo unzip -n NedFontsSymbolsOnly.zip -d ~/.local/share/fonts/
 sudo unzip -n SpaceMono.zip -d ~/.local/share/fonts/
-#   Enable Starship COPR repo
-sudo dnf copr enable atim/starship
-#   Enable Hyprland COPR repository
-sudo dnf copr enable solopasha/hyprland
+#   Install base packages
+sudo dnf -y in nano git curl bash bash-devel wget btop fastfetch firefox kitty dolphin code tmux sddm starship
 #   Install Hyprland and additional dependencies
-sudo dnf -y in hyprland hyprland-devel hyprpaper hypridle hyprlock hyprpicker hyprshot cliphist nwg-clipman waypaper waybar-git pavucontrol SwayNotificationCenter libnotify libnotify-devel starship
+sudo dnf -y in hyprland hyprland-devel hyprpaper hypridle hyprlock hyprpicker hyprshot cliphist nwg-clipman waypaper waybar-git pavucontrol SwayNotificationCenter libnotify libnotify-devel
 
-
-#sudo dnf -y in 
